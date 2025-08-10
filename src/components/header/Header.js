f0a91fea94f503888ed985aea933cd61b36a6bfe
rigolaz <68431157+RIGOLAZ@users.noreply.github.com>
2025-08-10 14:25:24 +0100

    Attempting Pi integration

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -30,7 +30,7 @@ const logo = (
       <img className={styles.pilo} src={"https://res.cloudinary.com/do8lyndou/image/upload/v1734023109/StorePi_mjubzf.svg"} alt="pilogo"/>
       <h6>Available soon</h6>
     </div>
-    <img className={styles.xmas} src="https://res.cloudinary.com/do8lyndou/image/upload/v1735091749/Chapeau-N%C3%B6el_vbieyp.png" alt="hat" />
+    {/* <img className={styles.xmas} src="https://res.cloudinary.com/do8lyndou/image/upload/v1735091749/Chapeau-N%C3%B6el_vbieyp.png" alt="hat" /> */}
   </div>
 );
 
